f38e4a0b9a157acd41a1895c41610320af4a3463
dlata29 <119111998+dlata29@users.noreply.github.com>
2025-01-21 14:34:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import OrdersPlacedStatisticsDashboard from "./Components/OrdersPlacedStatisticsDashboard";
-import Test from "./Components/test";
+
 function App() {
   return (
     <>
